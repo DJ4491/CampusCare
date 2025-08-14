c9a4104f625b890a1da5556ae6daa8d751895872
DJ <dhruvjoshi4498@gmail.com>
2025-08-14 21:46:59 +0530

    jsadded

--- static/script.js
+++ static/script.js
@@ -1,7 +1,7 @@
 const cards = document.querySelectorAll(".icon");
 cards.forEach((card) => {
   card.addEventListener("mousedown", () => {
-    card.style.transform = "scale(1.03)";
+    card.style.transform = "scale(1.04)";
     card.style.transition = "transform 0.2s ease";
   });
   card.addEventListener("mouseup", () => {
