4296f097aabf25475da293efceeea7e1ae17b048
DJ <dhruvjoshi4498@gmail.com>
2025-08-16 18:45:27 +0530

    touch

--- static/script.js
+++ static/script.js
@@ -1,10 +1,10 @@
 const cards = document.querySelectorAll(".icon");
 cards.forEach((card) => {
-  card.addEventListener("mousedown", () => {
+  card.addEventListener("touchstart", () => {
     card.style.transform = "scale(1.04)";
     card.style.transition = "transform 0.2s ease";
   });
-  card.addEventListener("mouseup", () => {
+  card.addEventListener("touchend", () => {
     card.style.transform = "scale(1)";
   });
 });
