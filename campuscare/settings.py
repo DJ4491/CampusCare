d7342d3efd7542d81ce5857674a3b69f4024b623
DJ <dhruvjoshi4498@gmail.com>
2025-09-14 12:42:24 +0530

    render

--- campuscare/settings.py
+++ campuscare/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-on!+kwxqk=_)wbz=16v)arjs+zz@&otf^x8pggjs=86j9yp8z9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["campuscare-2-ous0.onrender.com"]
 
 
 # Application definition
