f4d1c2a2e7c971f2785a403b2062804c9dcea6fa
Dhruv Joshi <100696814+DJ4491@users.noreply.github.com>
2025-09-24 23:26:43 +0530

    Remove unused AUTHENTICATION import from settings.py

--- campuscare/settings.py
+++ campuscare/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 import os
 from pathlib import Path
-from telnetlib import AUTHENTICATION
+# from telnetlib import AUTHENTICATION
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
