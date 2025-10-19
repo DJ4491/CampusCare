0048030b8e060e28a2ae05bc73f6a3c5a5819559
Dhruv Joshi <100696814+DJ4491@users.noreply.github.com>
2025-10-19 14:34:32 +0530

    ...

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -e
 
-PORT=${PORT:-8080}
 python manage.py migrate
 python manage.py collectstatic --noinput
+
+PORT=${PORT:-8080}
 exec gunicorn campuscare.wsgi:application --bind 0.0.0.0:$PORT --workers 3 --timeout 120
